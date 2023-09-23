960dd3fcf70e1c4a4e2957ecd2d1599891141249
panchali.c <panchalic3@gmail.com>
2023-09-23 23:53:39 +0530

    Roman to int using hash map

---
+++ src/main/java/romantoint.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+public class romantoint {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> store = new HashMap();
+        store.put('I',1);
+        store.put('V',5);
+        store.put('X',10);
+        store.put('L',50);
+        store.put('C',100);
+        store.put('D',500);
+        store.put('M',1000);
+        int length = s.length();
+        int number = store.get(s.charAt(length-1));
+        for (int i=length-2; i>=0; i--){
+            if(store.get(s.charAt(i))>=store.get(s.charAt(i+1))){
+                number=number + store.get(s.charAt(i));
+            }
+            else{
+                number=number - store.get(s.charAt(i));
+            }
+        }
+        return number;
+    }
+
+    public static void main(String[] args) {
+        romantoint obj = new romantoint();
+        System.out.println(obj.romanToInt("DCXXI"));
+    }
+}
