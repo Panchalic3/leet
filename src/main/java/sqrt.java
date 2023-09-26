cb2b0c039830c152f75b5c271ea83c00031f3960
panchali.c <panchalic3@gmail.com>
2023-09-26 21:43:05 +0530

    Square root using binary search

---
+++ src/main/java/sqrt.java
@@ -0,0 +1,38 @@
+public class sqrt {
+    public long mySqrt(int x) {
+        long  low = 1;
+        long high = x;
+        int ans = 0;
+        while(low <=high) {
+            if (low-high==1) {
+                ans=(int)high;
+                break;
+            }
+            else if(x==1){
+                ans=1;
+                break;
+            }
+            else {
+                long mid = (low + high) / 2;
+                long val = mid * mid;
+                if (val <= x) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                    ans=(int)high;
+                }
+            }
+        }
+            return ans;
+    }
+        public static void main(String[] args) {
+        sqrt o = new sqrt();
+        System.out.println(o.mySqrt(1));
+    }
+
+
+
+
+
+    }
+
