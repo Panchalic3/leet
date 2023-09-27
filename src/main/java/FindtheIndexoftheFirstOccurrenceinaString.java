1788e1fbf3d2cf43dfdc4c78d6f587bcaf9dd6f4
panchali.c <panchalic3@gmail.com>
2023-09-27 23:31:20 +0530

    find index of first occurance of string

---
+++ src/main/java/FindtheIndexoftheFirstOccurrenceinaString.java
@@ -0,0 +1,27 @@
+public class FindtheIndexoftheFirstOccurrenceinaString {
+    public int strStr(String haystack, String needle) {
+        int flag = -1;
+        for (int i = 0; i <=haystack.length()-needle.length(); i ++){
+            if (haystack.charAt(i) == needle.charAt(0)){
+                String check = haystack.substring(i,needle.length()+i);
+                if(check.equals(needle) && haystack.length()>needle.length()){
+                    flag = i;
+                    break;
+                }
+            }
+            else{
+                flag=-1;
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        FindtheIndexoftheFirstOccurrenceinaString f = new FindtheIndexoftheFirstOccurrenceinaString();
+        System.out.println(f.strStr("aaa","aaaa"));
+    }
+
+
+
+
+    }
