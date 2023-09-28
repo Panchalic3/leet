34f1ec15293a6ee555f6c95b8a7c1c69f92fd59a
panchali.c <panchalic3@gmail.com>
2023-09-28 23:25:36 +0530

    Write a function that reverses a string. The input string is given as an array of characters s.
    You must do this by modifying the input array

---
+++ src/main/java/ReverseString.java
@@ -0,0 +1,30 @@
+public class ReverseString {
+    public char[] reverseString(char[] s) {
+        int len = s.length;
+        if(len%2==0){
+            for(int i=0;i<=((len/2)-1); i++){
+                int back = len-i;
+                char temp =s[i];
+                s[i]=s[back-1];
+                s[back-1]=temp;
+            }
+        }
+        else{
+            for(int i=0;i<=(len/2); i++){
+                int back =  len-i;
+                char temp = s[i];
+                s[i]=s[back-1];
+                s[back-1]=temp;
+            }
+
+
+            }
+        return  s;
+    }
+
+    public static void main(String[] args) {
+        ReverseString o = new ReverseString();
+       char[] s = {'h','e','l','l','o'};
+        System.out.println(o.reverseString(s));
+    }
+}
