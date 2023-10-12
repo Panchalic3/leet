9f533da17560515be78d6eba2ff3d17337e0221b
panchali.c <panchalic3@gmail.com>
2023-10-12 19:06:49 +0530

    Find the Index of the First Occurrence in a String

--- src/main/java/FindtheIndexoftheFirstOccurrenceinaString.java
+++ src/main/java/FindtheIndexoftheFirstOccurrenceinaString.java
@@ -1,16 +1,15 @@
 public class FindtheIndexoftheFirstOccurrenceinaString {
     public int strStr(String haystack, String needle) {
         int flag = -1;
-        for (int i = 0; i <=haystack.length()-needle.length(); i ++){
-            if (haystack.charAt(i) == needle.charAt(0)){
-                String check = haystack.substring(i,needle.length()+i);
-                if(check.equals(needle) && haystack.length()>needle.length()){
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                String check = haystack.substring(i, needle.length() + i);
+                if (check.equals(needle) && haystack.length() > needle.length()) {
                     flag = i;
                     break;
                 }
-            }
-            else{
-                flag=-1;
+            } else {
+                flag = -1;
             }
         }
         return flag;
@@ -18,10 +17,8 @@ public class FindtheIndexoftheFirstOccurrenceinaString {
 
     public static void main(String[] args) {
         FindtheIndexoftheFirstOccurrenceinaString f = new FindtheIndexoftheFirstOccurrenceinaString();
-        System.out.println(f.strStr("aaa","aaaa"));
+        System.out.println(f.strStr("aaa", "aaaa"));
     }
 
 
-
-
-    }
+}
